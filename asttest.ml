1ba809bd5d8aeaa3dab84d3c69e8631ba73d80dc
Ashar Nadeem <asharnadeemc@gmail.com>
2021-04-25 18:43:01 -0400

    removed uneeded files and rename some stuff

--- asttest.ml
+++
@@ -1,7 +0,0 @@
-open Ast
-
-let _ = 
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-  let result = Ast.string_of_program program in
-  print_endline result
